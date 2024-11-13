37e85c7bf565b8e14e37c88b1edb0ee797940de0
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-11-13 18:37:25 +0330

    Rename project to resolve conflict with app name

--- alireza_fm_task/asgi.py
+++ alireza_fm_task/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for file_manager project.
+ASGI config for alireza_fm_task project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'file_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alireza_fm_task.settings')
 
 application = get_asgi_application()
--- alireza_fm_task/urls.py
+++ alireza_fm_task/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for file_manager project.
+URL configuration for alireza_fm_task project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
