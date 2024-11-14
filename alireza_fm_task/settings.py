0a882e215abbbb190878961558e6cd6894983ee5
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-11-14 14:55:39 +0330

    Change database settings to connect to postgres database

--- alireza_fm_task/settings.py
+++ alireza_fm_task/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'alireza_fm_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'filemanagerdb',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
