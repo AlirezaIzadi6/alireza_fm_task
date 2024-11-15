80a5fb1dce1d9ee91ed23a56c2808cc3a04f0992
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-11-15 18:25:39 +0330

    Make create_date and update_date fields set automatically

--- file_manager/models.py
+++ file_manager/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Folder(models.Model):
     name = models.CharField(max_length=100)
     path = models.CharField(max_length=255)
-    create_date = models.DateTimeField()
-    update_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     parent_folder = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
 class MediaFile(models.Model):
@@ -12,6 +12,6 @@ class MediaFile(models.Model):
     path = models.CharField(max_length=255)
     type = models.CharField(max_length=5)
     size = models.IntegerField()
-    create_date = models.DateTimeField()
-    update_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
