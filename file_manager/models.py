b08b1db49d5e2416ed470d1a9172372fbe342546
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-11-15 20:36:30 +0330

    Add creator field to Folder and MediaFile models

--- file_manager/models.py
+++ file_manager/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from accounts.models import CustomUser
+
 class Folder(models.Model):
     name = models.CharField(max_length=100)
     path = models.CharField(max_length=255)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
     parent_folder = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    creator = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
 
 class MediaFile(models.Model):
     name = models.CharField(max_length=100)
@@ -15,3 +18,4 @@ class MediaFile(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    creator = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
