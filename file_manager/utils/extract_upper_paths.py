3f155280f40bafb3190724901c53e9311691b346
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-12-06 08:43:05 +0330

    Add links to breadcrumb

--- file_manager/utils/extract_upper_paths.py
+++ file_manager/utils/extract_upper_paths.py
@@ -3,10 +3,10 @@ from file_manager.models import Folder
 def extract_upper_folders(current):
     if current == None or current.parent_folder == None:
         return []
-    print(current.parent_folder)
     result = []
     current_parent = current.parent_folder
     while current_parent != None:
         result.append(current_parent)
         current_parent = current_parent.parent_folder
+    result.reverse()
     return result
